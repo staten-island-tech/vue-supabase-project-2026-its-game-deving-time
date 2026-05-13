ff42ba424a8ee7e5453506d047057f4316013728
Nom075 <graytonl@nycstudents.net>
2026-05-13 14:02:17 -0400

    wqdwe

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,4 +2,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  css: ['@/assets/main.css']
 })
+
+
+import type { Config } from 'tailwindcss'
+
+/* export default {
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./app.vue"
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [require('daisyui')],
+} satisfies Config */
