35f29e13b2a6d8f4d4fd4742c84509648111f815
Nom075 <graytonl@nycstudents.net>
2026-05-19 17:57:25 -0400

    cookin

---
+++ app/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class = "flex flex-col justify-center">
+        <h3 class = "text-blue-600"><b>This is the login page</b></h3>
+        <p v-for="players in data">{{players}}</p>
+    </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+
+const {data, error} = await supabase.from(`Players`).select('*')
+if (error) throw error;
+console.log(data)
+
+</script>
+
+<style scoped>
+</style>
