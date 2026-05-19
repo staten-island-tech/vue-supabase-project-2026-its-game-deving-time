9446e76df4081084243816b2a4ec84686e906fc2
Nom075 <graytonl@nycstudents.net>
2026-05-19 14:02:34 -0400

    se

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,25 +1,19 @@
 <template>
     <div class = "flex justify-center">
-        <h3 class = "text-blue-600"><b>Project Name</b></h3>
-        <input type="file" class="file-input" />
+      <h3 class = "text-blue-600"><b>Project Name</b></h3>
+        <div>
+
+        </div>
     </div>
-    <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
 </template>
 
 <script setup>
+const supabase = useSupabaseClient()
+
+const {data, error} = await supabase.from(`players`).select('*').eq('id', playerId).single()
+if (error) throw error;
+console.log(data)
+
 </script>
 
 <style scoped>
