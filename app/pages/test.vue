66e15482ecb6ce6c4429d159d6befaae38ce35a2
Chaojie1 <chaojiew2@nycstudents.net>
2026-05-21 14:01:43 -0400

    guh

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -17,7 +17,8 @@
         Hitbox: RAPIERtype.RigidBody,
         Box: THREE.Box3,
         OBBLocal: OBB,
-        OBBWorld: OBB
+        OBBWorld: OBB,
+        Collider:RAPIERtype.Collider
     }
     type enemy = {
         Speed: number,
@@ -74,7 +75,7 @@
             cube.updateMatrixWorld();
             obbWorld.copy(obbLocal).applyMatrix4(cube.matrixWorld);
 
-            createdObjects.push({Visual: cube, Hitbox: hitboxdesc, Box: box3, OBBLocal: obbLocal, OBBWorld: obbWorld})
+            createdObjects.push({Visual: cube, Hitbox: hitboxdesc, Box: box3, OBBLocal: obbLocal, OBBWorld: obbWorld,Collider:collider})
             if (enemydata !== undefined){
                 enemies.push({
                     Visual: cube, 
@@ -84,12 +85,25 @@
                     MaxHP: enemydata.MaxHP,
                     Box: box3,
                     OBBLocal: obbLocal,
-                    OBBWorld: obbWorld
+                    OBBWorld: obbWorld,
+                    Collider:collider
                 })
             }
             return {Collider: collider, Body: hitboxdesc, Visual: cube, Box: box3, OBBLocal: obbLocal, OBBWorld: obbWorld}
         }
+        function removeObject(scene:THREE.Scene, world:RAPIERtype.World, mesh:THREE.Mesh, rigidBody:RAPIERtype.RigidBody, collider:RAPIERtype.Collider) {
+            world.removeCollider(collider, true);
+            world.removeRigidBody(rigidBody);
 
+            scene.remove(mesh);
+            mesh.geometry.dispose();
+
+            if (Array.isArray(mesh.material)) {
+                mesh.material.forEach(m => m.dispose());
+            } else {
+                mesh.material.dispose();
+            }
+        }
     onMounted(()=>{
         const brij = new Audio('/bruh.mp3')
         const world = new RAPIER.World({x:0, y:-9.81, z:0})
@@ -125,8 +139,9 @@
         const keysdown: Record<string,boolean> = {}
         window.addEventListener("keydown", (key: KeyboardEvent) => keysdown[key.code] = true)
         window.addEventListener("keyup", (key: KeyboardEvent) => keysdown[key.code] = false)
+        for (let i=1;i<20;i++){
         createObject({x: 0, y: 0, z: 0}, {x: randInt(-25,25), y: randInt(30,60), z: randInt(-25,25)}, {x:1,y:1,z:1}, 0xFFFFFF, world, scene, 1, "rect", "evil.png", {Speed:1, Health:1, MaxHP:1})     
-        
+        }
         function checkCollision(
             object: {OBBLocal: OBB, OBBWorld: OBB, Visual: THREE.Mesh},
             plr: {OBBLocal: OBB, OBBWorld: OBB, Visual: THREE.Mesh}
@@ -137,11 +152,13 @@
             plr.OBBWorld.copy(plr.OBBLocal).applyMatrix4(plr.Visual.matrixWorld);
             return object.OBBWorld.intersectsOBB(plr.OBBWorld);
         }
-
+        let hp = 3
+        let iframes = 0
         let reloading = false
         const animate = (): void => {
             requestAnimationFrame(animate)
             world.step()
+            iframes = Math.max(0,iframes-1)
             createdObjects.forEach((x)=>{
                 const pos = x.Hitbox.translation()
                 const rot = x.Hitbox.rotation()
@@ -156,46 +173,68 @@
                 .normalize()
                 .multiplyScalar(x.Speed/10);
                 x.Hitbox.applyImpulse(lookVector, true)
-                if (checkCollision({OBBLocal:x.OBBLocal, OBBWorld:x.OBBWorld, Visual:x.Visual}, {OBBLocal:plr.OBBLocal, OBBWorld:plr.OBBWorld, Visual:plr.Visual}) && !reloading){
-                    reloading = true
-                    location.reload()
+                if (checkCollision({OBBLocal:x.OBBLocal, OBBWorld:x.OBBWorld, Visual:x.Visual}, {OBBLocal:plr.OBBLocal, OBBWorld:plr.OBBWorld, Visual:plr.Visual}) && !reloading && iframes==0){
+                    iframes=120
+                    hp--
+                    if (hp==0){
+                        reloading = true
+                        enemies.forEach(enemy=>{
+                            removeObject(scene,world,enemy.Visual,enemy.Hitbox,enemy.Collider)
+                        })
+                        for (let i=1;i<100;i++){
+                            createObject({x: 0, y: 0, z: 0}, {x: plr.Visual.position.x+randInt(-1,1)/10000, y: plr.Visual.position.y+randInt(-1,1)/10000, z: plr.Visual.position.z+randInt(-1,1)/10000}, {x:0.3,y:0.3,z:0.3}, 0xFFFFFF, world, scene, 1, "a", "rb4.png")    
+                        }
+
+                    }
                 }
             })
-            
+            console.log(hp)
+            if (hp==3){
+                plr.Visual.material.color.setRGB(1,1,1)
+            }else if(hp ==2){
+                plr.Visual.material.color.setRGB(1,0.3,0.3)
+            }else if(hp==1){
+                plr.Visual.material.color.setRGB(1,0,0)
+            }else{
+                createObject({x: 0, y: 0, z: 0}, {x: plr.Visual.position.x+randInt(-1,1)/10000, y: plr.Visual.position.y+randInt(-1,1)/10000, z: plr.Visual.position.z+randInt(-1,1)/10000}, {x:0.3,y:0.3,z:0.3}, 0xFFFFFF, world, scene, 1, "a", "rb4.png")    
+            }
             const pos2 = ground.Body.translation()
             const rot2 = ground.Body.rotation()
             ground.Visual.position.set(pos2.x, pos2.y, pos2.z)
             ground.Visual.quaternion.set(rot2.x, rot2.y, rot2.z, rot2.w)
+            if (hp>0){
+                const dir = new THREE.Vector3();
+                if (keysdown['KeyW']) dir.z -= 1;
+                if (keysdown['KeyS']) dir.z += 1;
+                if (keysdown['KeyA']) dir.x -= 1;
+                if (keysdown['KeyD']) dir.x += 1;
+                const pos = plr.Body.translation()
 
-            const dir = new THREE.Vector3();
-            if (keysdown['KeyW']) dir.z -= 1;
-            if (keysdown['KeyS']) dir.z += 1;
-            if (keysdown['KeyA']) dir.x -= 1;
-            if (keysdown['KeyD']) dir.x += 1;
-            const pos = plr.Body.translation()
+                const origin = {
+                    x: pos.x,
+                    y: pos.y - 0.9,
+                    z: pos.z
+                }
 
-            const origin = {
-                x: pos.x,
-                y: pos.y - 0.9,
-                z: pos.z
+                const ray = new RAPIER.Ray(origin, {x: 0, y: -1, z: 0})
+                const hit = world.castRay(ray, 0.15, true)
+                const grounded = hit !== null
+                if (keysdown['Space'] && grounded) {
+                    plr.Body.applyImpulse({x: 0, y: 1, z: 0}, true)
+                }
+                dir.normalize().multiplyScalar(0.0025);
+                const force = new RAPIER.Vector3(dir.x * 50, 0, dir.z * 50)
+                plr.Body.applyImpulse(force, true)
             }
-
-            const ray = new RAPIER.Ray(origin, {x: 0, y: -1, z: 0})
-            const hit = world.castRay(ray, 0.15, true)
-            const grounded = hit !== null
-            if (keysdown['Space'] && grounded) {
-                plr.Body.applyImpulse({x: 0, y: 1, z: 0}, true)
-            }
-            dir.normalize().multiplyScalar(0.0025);
-            const force = new RAPIER.Vector3(dir.x * 50, 0, dir.z * 50)
-            plr.Body.applyImpulse(force, true)
             const p = plr.Body.translation()
             const r = plr.Body.rotation()
             plr.Visual.position.set(p.x, p.y, p.z)
             plr.Visual.quaternion.set(r.x, r.y, r.z, r.w)
+            if (hp>0){
             camera.position.x = plr.Visual.position.x
             camera.position.y = plr.Visual.position.y + 6
             camera.position.z = plr.Visual.position.z + 6
+            }
             renderer.render(scene, camera)
         }
         animate()
